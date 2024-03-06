b057ef09a40428daf14508a0ac0a5418898fbef1
Crapa-Laptop <crapa36@gmail.com>
2024-03-06 11:31:32 +0900

    2차시 마무리

--- STL.cpp
+++ STL.cpp
@@ -9,46 +9,52 @@
 
 #include <iostream>
 #include "save.h"
+
+#include <format>
+#include <random>
 using namespace std;
+default_random_engine dre;
+uniform_int_distribution uid{ 1000,9999 };
+
 
 //main을 수정하지 않고 의도대로 실행되게 change를 선언하고 정의하라
 
-void change(int& a, int& b) {// 1,2 스와핑
+class Dog {//클래스에서 이정도만 쓰면 훌륭하다.  
+private:
+    int age;
+
+public:
+    Dog(int age) : age(age) {};
+    
+    friend ostream& operator<<(ostream& os, const Dog& dog) {
+        return os << dog.age;
+    }
+    operator int() {
+        return age;
+    }
+};
+
+template <class T>
+void change(T& a, T& b) {// 1,2 스와핑
     int temp{ a };
     a = b;
     b = temp;
 }
 
-class Dog {
-public:
-    Dog(int a) {
-        num = a;
-    }
+int main(){
+    int a[1000];
+    
+    for (int& num : a)
+        num = uid(dre);
 
-    /*Dog operator=(Dog& ref){
-        num = ref.num;
-    }*/
-    /*void change(Dog& a, Dog& b) {
-        Dog temp{ a };
-        a = b;
-    }*/
-    operator int() {
-        return(num);
-    }
-private:
-    int num;
-
-};
-
-void change(Dog& a, Dog& b);
-
-int main()
-{
-    Dog a{ 1 }, b{ 2 };
-    change(a, b);
-    cout << a << ", " << b << endl;
-
-    //save("save.h");
-    //save("save.cpp");
-    save("STL.cpp");
+    for (int num : a)
+        cout << format("{:8}", num);
+    int largest = 0;
+    for (int num : a)
+        if (num > largest)largest = num;
+    cout << endl << format("{:8}", largest);
+    save("stl.cpp");
 }
+
+
+
